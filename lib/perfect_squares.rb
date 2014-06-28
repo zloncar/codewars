293e008a8d536cb855da606117c181f3dd68ecc1
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2014-06-28 16:12:38 +0100

    Or one line job.

--- lib/perfect_squares.rb
+++ lib/perfect_squares.rb
@@ -2,12 +2,13 @@ require 'test/unit'
 
 class PerfectSquares
   def self.get_squares(array)
-    squares = []
-    array.each do |x|
-      sqrt = Math.sqrt(x)
-      squares << x if sqrt - sqrt.floor == 0
-    end
-    squares
+    # squares = []
+    # array.each do |x|
+    #   sqrt = Math.sqrt(x)
+    #   squares << x if sqrt - sqrt.floor == 0
+    # end
+    # squares
+    array.select {|x| x % (Math.sqrt x) == 0}.sort.uniq
   end
   puts get_squares(1..16)
 end
