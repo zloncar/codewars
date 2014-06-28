2915564e5a156a08e9f0ffd0408658a19d9faed5
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2014-06-28 16:08:09 +0100

    Tidy.

--- lib/perfect_squares.rb
+++ lib/perfect_squares.rb
@@ -1,3 +1,5 @@
+require 'test/unit'
+
 class PerfectSquares
   def self.get_squares(array)
     squares = []
@@ -7,15 +9,11 @@ class PerfectSquares
     end
     squares
   end
+  puts get_squares(1..16)
 end
-# get_squares(1..16)
 
 # perfect_squares = Proc.new do |i|
 #   sqrt = Math.sqrt(i)
 #   sqrt - sqrt.floor == 0
 # end
 # puts (1..17).select(&perfect_squares)
-
-# # Test.assert_equal get_squares((1..16)), [1,4,9,16])
-# assert_equal(get_squares((1..100)), [1,4,9,16,25,36,49,64,81,100])
-# # Test.assert_equal [1,4,16], get_squares([4,1,16,1,10,35,22])
