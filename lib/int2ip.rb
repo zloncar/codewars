b6a62ed6a95ff0b070d1d5aed18179ee90c70402
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2014-07-03 14:23:37 +0100

    Add codewars clever solutions for int2ip.

--- lib/int2ip.rb
+++ lib/int2ip.rb
@@ -8,3 +8,12 @@ def int32_to_ip(int32)
 end
 
 puts int32_to_ip(2154959208) # => 128.114.17.104
+
+#clever
+# def int32_to_ip(int32)
+#   [24, 16, 8, 0].collect {|b| (int32 >> b) & 255}.join('.')
+# end
+
+# def int32_to_ip(int32)
+#   [int32].pack('N').unpack('C*') * '.'
+# end
