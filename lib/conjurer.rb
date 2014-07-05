4ece4d207dd3247d548feeb42542a7db7ee0a631
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2014-07-05 13:59:16 +0100

    Conjurer: 2 codewars solutions.

--- lib/conjurer.rb
+++ lib/conjurer.rb
@@ -7,3 +7,20 @@ end
 bar = lambda { (1..4).to_a }
 Conjurer.conjure(:blah, bar)
 puts Conjurer.new.blah
+
+# codewars solutions
+# class Conjurer
+#   class << self
+#     def conjure(name, impl)
+#       define_method(name, &impl)
+#     end
+#   end
+# end
+
+# class Conjurer
+#   def self.conjure(name, method)
+#     Conjurer.send(:define_method, name) do
+#       method
+#     end
+#   end
+# end
