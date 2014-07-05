ea8356721fa1d8a40a8375a1b96d2782474bee50
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2014-07-05 13:18:04 +0100

    Amend to work on codewars.

--- lib/conjurer.rb
+++ lib/conjurer.rb
@@ -1,11 +1,12 @@
 class Conjurer
 
-  def self.conjure(name, &lambda)
-    define_method "#{name}" do
-      yield
+  def self.conjure(name, foo)
+    define_method name do
+      foo.call
     end
   end
 end
 
-Conjurer.conjure('blah') { (1..5).to_a }
+bar = lambda { (1..4).to_a }
+Conjurer.conjure(:blah, bar)
 puts Conjurer.new.blah
